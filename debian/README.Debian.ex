3418d03ea7f60daaa3f032444c39e9e35d0b772e
Phil Karn <karn@ka9q.net>
2026-05-17 20:09:38 -0400

    clean out deadwood; install udev rules

--- debian/README.Debian.ex
+++
@@ -1,6 +0,0 @@
-libfobos for Debian
-------------------
-
-<Possible notes regarding this package - if none, delete this file.>
-
- -- Phil Karn <karn@sdr>  Sat, 02 May 2026 15:47:24 -0700
--- debian/README.source.ex
+++
@@ -1,9 +0,0 @@
-libfobos for Debian
-------------------
-
-<This file describes information about the source package, see Debian policy
-manual section 4.14. You WILL either need to modify or delete this file.>
-
-
-
- -- Phil Karn <karn@sdr>  Sat, 02 May 2026 15:47:24 -0700
